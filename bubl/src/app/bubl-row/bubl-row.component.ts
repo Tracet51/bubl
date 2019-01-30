343b844c213c238f0b56366a9c901c2ba35eca42
Trace Tschida <tracetschida@TTSCHIDA-MBP-DC.local>
2019-01-30 17:08:00 -0600

    Formating

--- bubl/src/app/bubl-row/bubl-row.component.ts
+++ bubl/src/app/bubl-row/bubl-row.component.ts
@@ -6,12 +6,12 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./bubl-row.component.css']
 })
 export class BublRowComponent implements OnInit {
-  @Input()yesVote = 0;  // makes it bindable
-  @Input()noVote = 0;
-  @Input()total = 0;
-  @Input()daysRemaining = 0;
-  @Input()summary = "";
-  @Input()description = "";
+  @Input() yesVote = 0;  // makes it bindable
+  @Input() noVote = 0;
+  @Input() total = 0;
+  @Input() daysRemaining = 0;
+  @Input() summary = "";
+  @Input() description = "";
 
   constructor() {
 
