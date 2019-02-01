dff7d06550d11d90d9869c79e2ff6f8ae510acaa
Trace Tschida <tracetschida@TTSCHIDA-MBP-DC.local>
2019-02-01 10:37:16 -0600

    changed details

--- bubl/src/app/bubl-detail/bubl-detail.component.ts
+++ bubl/src/app/bubl-detail/bubl-detail.component.ts
@@ -12,7 +12,7 @@ import { BublComment } from '../DTOs/BublComment';
 export class BublDetailComponent implements OnInit {
   bubl: Bubl;
   id: number;
-  currentFinID: number = 3;
+  currentFinID: number = 2;
   commentString = "";
 
   constructor(
@@ -34,17 +34,29 @@ export class BublDetailComponent implements OnInit {
   }
 
   upVote() {
-    this.bubl.yes += 1
+    this.bubl.yes += 1;
+    document.getElementById("voted-alert-bar").style.height = "100px";
+    this.returnToFeed();
   }
 
-  downVote(){
-    this.bubl.no += 1
+  downVote() {
+    this.bubl.no += 1;
+    document.getElementById("voted-alert-bar").style.height = "100px";
+
+    this.returnToFeed();
+  }
+
+  returnToFeed() {
+    setTimeout(() => {
+      this.router.navigateByUrl('/bubl-feed-page');
+    }, 1000);
+    
   }
 
   createNewComment() {
     let newComment: BublComment = {
       comment: this.commentString,
-      id: Math.floor(Math.random()*10000),
+      id: Math.floor(Math.random() * 10000),
       date: new Date(),
       finID: this.currentFinID,
       bublID: this.bubl.id
